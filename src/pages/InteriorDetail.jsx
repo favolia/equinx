5f53b4538ae44653f4fef6623ea8c532f742be26
dais <daisszo1455@gmail.com>
2024-01-04 01:50:57 +0800

    update interior detail

--- src/pages/InteriorDetail.jsx
+++ src/pages/InteriorDetail.jsx
@@ -49,7 +49,32 @@ const InteriorDetail = () => {
                 </div>
             </header>
 
-            <div className=''>ldnsdksd</div>
+            <div className='w-full text-white px-4 pt-10 pb-20'>
+                <div className='w-full flex justify-between items-center'>
+                    <h1 className='hidden lg:flex font-soligant text-6xl'>Catalog 01</h1>
+                    <div className='w-full lg:w-fit flex flex-col items-center lg:items-start gap-y-4'>
+                        <div className='flex justify-between w-full lg:w-fit'>
+                            <h2 className='text-2xl lg:text-4xl font-soligant'>About</h2>
+                            <h2 className='lg:hidden text-2xl lg:text-4xl font-soligant'>Catalog 01</h2>
+                        </div>
+                        <p className='text-center lg:text-left lg:text-xs w-52 lg:w-52 mt-6 lg:mt-0'>
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+                        </p>
+                    </div>
+                </div>
+
+                <div className='w-full lg:px-8 mt-12 lg:mt-20'>
+                    <Image className='w-full rounded-small overflow-hidden' src={'/assets/images/Firefox_Screenshot_2023-12-28T13-49-13.095Z.png'} width={800} height={800} priority quality={100} loading='eager' alt='interior home page' />
+                </div>
+
+                <div className='w-full grid grid-rows-4 lg:grid-rows-2 grid-cols-1 lg:grid-cols-2 place-items-center lg:gap-y-10 mt-12 lg:mt-48 px-4 lg:px-0'>
+                    <Image className='w-96 order-1 lg:translate-x-5 lg:-translate-y-16' src={'/assets/images/Firefox_Screenshot_2023-12-28T13-52-06.276Z.png'} width={800} height={800} priority quality={100} loading='eager' alt='interior section 1' />
+                    <Image className='w-96 order-2' src={'/assets/images/Screenshot_4.png'} width={800} height={800} priority quality={100} loading='eager' alt='interior section 2' />
+                    <Image className='w-96 order-4 lg:order-3 lg:-translate-x-5 lg:translate-y-16' src={'/assets/images/Firefox_Screenshot_2023-12-28T13-56-21.505Z.png'} width={800} height={800} priority quality={100} loading='eager' alt='interior section 2' />
+                    <Image className='w-96 order-3 lg:order-4' src={'/assets/images/Firefox_Screenshot_2023-12-28T13-57-55.416Z.png'} width={800} height={800} priority quality={100} loading='eager' alt='interior section 2' />
+                </div>
+            </div>
+
         </main>
     )
 }
