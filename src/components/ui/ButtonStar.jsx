c4e6a13ba13f16f99cd918f2856531d1251dc703
dais <daisszo1455@gmail.com>
2024-01-10 20:21:37 +0800

    update revisi 2 (about)

--- src/components/ui/ButtonStar.jsx
+++ src/components/ui/ButtonStar.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import StarRing from './StarRing';
 import { cn } from '@/lib/utils';
 
-const ButtonStar = ({ text = 'Contact Us', href = '/contact-us', color = 'black', style = '' }) => {
+const ButtonStar = ({ text = 'Contact Us', href = '/contact-us', color = 'black', style = '', starSize }) => {
     return (
         <Link href={href} className={cn(`gap-x-5 h-10 w-48 border !border-${color} text-${color} rounded-2xl flex justify-center items-center`, style)}>
-            <div className='w-7'>
+            <div className={cn('w-7', starSize)} >
                 <StarRing color={`fill-${color}`} />
             </div>
             {text}
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -8,60 +8,67 @@ import Flower from '@/components/ui/Flower';
 
 const ContactUs = () => {
     return (
-        <main className='h-screen w-full flex flex-col lg:flex-row text-white'>
-            <div className='w-full h-full bg-sky-blue'>
+        <main className='min-h-screen lg:h-screen w-full flex flex-col lg:flex-row text-white'>
+            <div className='w-full h-full bg-sky-blue pb-28 lg:pb-3'>
                 <div className='block w-full p-4 lg:hidden'>
                     <NavbarMobile />
                 </div>
 
-                <div className='w-full hidden lg:flex flex-col pt-6 pl-10 pr-5 gap-y-16'>
+                <div className='w-full hidden lg:flex flex-col pt-6 pl-12 pr-5'>
                     <div className='w-full flex justify-between items-center'>
                         <Link href={'/'}>Equinx</Link>
                         <ButtonStar text='Back To Home' href='/' color='white' />
                     </div>
-
-                    <div className='w-full flex flex-col items-center justify-center gap-y-16'>
-
-                        <div className='w-full flex justify-between items-center'>
-                            <p className='text-xs'>2024 - Contact</p>
-                            <p className='text-xs'>Your Trustworthy Partner from Plan to Polish</p>
-                        </div>
-
-                        <p className='text-sm'>Impeccable</p>
-
-                        <h1 className='text-8xl font-soligant self-start'>Contact Us</h1>
-
-                    </div>
-
                 </div>
 
-                <div className='w-full -translate-x-5 flex justify-center items-center lg:hidden'>
-                    <h1 className='font-soligant flex items-center text-3xl lg:text-8xl gap-x-5 lg:gap-x-32 mt-10 lg:mt-5'>
-                        Contact
-                        <div className='w-5'>
-                            <Flower color='fill-white' />
-                        </div>
-                        Us
-                    </h1>
-                </div>
-
-                <div className='w-full flex flex-col lg:flex-row gap-y-5 px-4 lg:pr-0 lg:pl-10 mt-14 lg:mt-20'>
-                    <div className='w-full flex flex-col gap-y-5'>
-                        <h1 className='font-soligant text-5xl'>Drop Us A Line</h1>
-                        <Link href={'#'} className='lg:text-sm'>equinx000@gmail.com</Link>
+                <div className='mt-5 px-4 lg:px-0 lg:mt-10'>
+                    <div className='w-full flex justify-between items-center lg:pl-12 lg:pr-5'>
+                        <p className='text-xs lg:text-base font-light'>2024</p>
+                        <p className='hidden lg:block lg:text-sm xl:text-base font-light'>Your Trustworthy Partner from Plan to Polish</p>
                     </div>
 
-                    <Link href={'#'} className='self-end lg:self-start translate-y-12 lg:translate-y-0 lg:translate-x-10'>
-                        <div className='h-24 w-24 flex justify-center items-center bg-corn-yellow rounded-full'>
-                            <IoMdMail className='text-4xl' />
-                        </div>
-                    </Link>
+                    <div className='my-11 w-full flex justify-center items-center lg:my-12 xl:my-14'>
+                        <Link href={'/contact-us'} className='text-4xl lg:text-3xl xl:text-4xl pb-1 border-b border-white text-white font-soligant px-1'>
+                            Contact Us
+                        </Link>
+                    </div>
 
+                    <div className='lg:px-10 flex flex-col justify-center items-start gap-y-10'>
+                        <h2 className='w-56 font-light lg:font-normal lg:text-3xl xl:text-4xl lg:w-96'>
+                            Lets shape your online journey collaboratively.
+                        </h2>
+
+                        <p className='text-sm lg:text-xl xl:text-2xl font-light'>Drop Us A Line</p>
+                    </div>
+
+                    <div className='lg:px-10 mt-10'>
+                        <Link href={'#'}>
+                            <h1 className='text-2xl lg:text-5xl xl:text-6xl font-light pb-2 lg:pb-5 border-b border-b-white w-fit'>
+                                Equinx000@gmail.com
+                            </h1>
+                        </Link>
+                    </div>
+
+                    <div className='hidden lg:block lg:mt-8 xl:mt-10'>
+                        <ButtonStar text='All Rights Reserved' color='white' starSize={'w-12'} style='border-none pl-10 w-fit' />
+                    </div>
                 </div>
 
             </div>
 
-            <div className='w-full lg:w-[50rem] h-full bg-[url(/assets/images/chris-galbraith-7XAM0J3dNQM-unsplash.jpg)] bg-cover bg-center'></div>
+            <div className='w-full flex justify-center items-end pb-6 lg:w-[50rem] h-80 lg:h-full bg-[url(/assets/images/chris-galbraith-7XAM0J3dNQM-unsplash.jpg)] bg-cover bg-center relative'>
+                <div className='absolute top-0 right-10 lg:top-auto lg:bottom-20 lg:left-0 -translate-y-10 lg:translate-y-0 lg:-translate-x-10'>
+                    <Link href={'#'} className=''>
+                        <div className='h-20 w-20 flex justify-center items-center bg-[#E7D8AE] text-[#5D95A3] rounded-full'>
+                            <IoMdMail className='text-3xl' />
+                        </div>
+                    </Link>
+                </div>
+
+                <div className='lg:hidden'>
+                    <ButtonStar href='#' text='All Rights Reserved' color='black' starSize={'w-12'} style='w-fit text-charcoal border-transparent' />
+                </div>
+            </div>
         </main>
     )
 }
