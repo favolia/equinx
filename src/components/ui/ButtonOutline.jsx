5aa25fc5ff017181becdbab9bde0d9b4a2c6dbf7
dais <daisszo1455@gmail.com>
2024-01-11 14:21:47 +0800

    small fix for href

--- src/components/ui/ButtonOutline.jsx
+++ src/components/ui/ButtonOutline.jsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 
 const ButtonOutline = ({ color = 'white', text = 'Visit Site', style, href = '#' }) => {
     return (
-        <Link href={href} className={cn(`gap-x-5 h-10 w-48 border !border-${color} text-${color} rounded-2xl flex justify-center items-center`, style)}>
+        <Link href={href} target="_blank" className={cn(`gap-x-5 h-10 w-48 border !border-${color} text-${color} rounded-2xl flex justify-center items-center`, style)}>
             {text}
         </Link>
     )
